298e14947665b2675924e68cc9cea289f89ad2b6
Gregori <gregori.figueira61@gmail.com>
2025-05-15 22:33:33 -0300

    ajuste na arquitetura, implementando jwt, atualizado .net para versão 9, retirado algumas views não utilizadas, iniciado adição de usuario e login

---
+++ WebApplication1/Core/Intefaces/IAutenticacaoServico.cs
@@ -0,0 +1,9 @@
+﻿using WebApplication1.Core.Entidades;
+
+namespace WebApplication1.Core.Intefaces
+{
+    public interface IAutenticacaoServico
+    {
+        string GerarToken(Usuario usuario);
+    }
+}
